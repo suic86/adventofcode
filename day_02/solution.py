b645c989ccb72c6f36f73d4e18a385c16597e1f3
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-02 16:54:21 +0100

    Day 02 - Solutions

---
+++ day_02/solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from re import findall
+
+
+def parse_data():
+    with open("input.data") as f:
+        data = f.read()
+    return findall(r"(\d+)-(\d+) ([a-z]): ([a-z]+)", data)
+
+
+def solution_01():
+    return sum(int(l) <= p.count(c) <= int(u) for l, u, c, p in parse_data())
+
+
+def solution_02():
+    result = 0
+    for f, s, c, p in parse_data():
+        try:
+            if (p[int(f) - 1], p[int(s) - 1]).count(c) == 1:
+                result += 1
+        except IndexError:
+            pass
+    return result
+
+
+if __name__ == "__main__":
+    print(solution_01())
+    print(solution_02())
