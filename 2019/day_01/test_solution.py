5783b089851828ad328ece4293a94fff26e11ee8
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-04 15:17:57 +0100

    2019 Day 01 - Solutions

---
+++ 2019/day_01/test_solution.py
@@ -0,0 +1,19 @@
+import pytest
+
+from solution import fuel, solution_01, total_fuel
+
+
+def test_solution_01():
+    assert solution_01() == 3159380
+
+
+@pytest.mark.parametrize(
+    "module,fuel_weight", [(12, 2), (14, 2), (1969, 654), (100756, 33583)]
+)
+def test_fuel(module, fuel_weight):
+    assert fuel(module) == fuel_weight
+
+
+@pytest.mark.parametrize("module,fuel", [(14, 2), (1969, 966), (100756, 50346)])
+def test_total_fuel(module, fuel):
+    assert total_fuel(module) == fuel
