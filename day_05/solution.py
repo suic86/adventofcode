81ce3b56d2bedab88c8b3bbd40339ef1478ec273
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-05 15:22:32 +0100

    Day 05 - Solutions - refactoring

--- day_05/solution.py
+++ day_05/solution.py
@@ -18,16 +18,21 @@ def missing_seat(seats):
 
 def read_data(path="input.data"):
     with open(path) as fobj:
-        return [line.rstrip() for line in fobj]
+        return list(map(str.rstrip, fobj))
 
 
-def solution_01(path="input.data"):
-    return max(map(seat, read_data(path)))
+def seats(path="input.data"):
+    return map(seat, read_data(path))
 
 
-def solution_02(path="input.data"):
-    return missing_seat(map(seat, read_data(path)))
+def solution_01():
+    return max(seats())
+
+
+def solution_02():
+    return missing_seat(seats())
 
 
 if __name__ == "__main__":
     print(solution_01())
+    print(solution_02())
