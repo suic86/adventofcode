51d396aa7165ccfeea497d6a028ae839d0569708
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-06 15:29:45 +0100

    Day 06 - Solutions - refactoring

--- day_06/solution.py
+++ day_06/solution.py
@@ -3,22 +3,23 @@ from functools import reduce
 
 
 def read_data(path="input.data"):
-    data = []
     with open(path) as fobj:
         group = []
         for row in map(str.rstrip, fobj):
             if row:
                 group.append(row)
                 continue
-            data.append(group)
+            yield group
             group = []
         if group:
-            data.append(group)
-    return data
+            yield group
+
+
+def solution_01_old():
+    return sum(len(reduce(set.union, map(set, group))) for group in read_data())
 
 
 def solution_01():
-    # return sum(len(reduce(set.union, map(set, group))) for group in read_data())
     return sum(len(set().union(*group)) for group in read_data())
 
 
---
+++ day_06/test_solution.py
@@ -0,0 +1,11 @@
+from solution import solution_01, solution_02, solution_01_old
+
+def test_solution_01_old():
+    assert solution_01_old() == 6630
+
+def test_solution_01():
+    assert solution_01() == 6630
+
+
+def test_solution_02():
+    assert solution_02() == 3437
