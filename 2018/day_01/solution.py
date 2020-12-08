8a884b632d806feafa499b2a027d6a321b817173
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-09 00:53:01 +0100

    2018 - Day 01 - Solutions

---
+++ 2018/day_01/solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from itertools import accumulate, cycle
+
+
+def read_data(path="input.data"):
+    with open(path) as source_file:
+        return list(map(int, source_file))
+
+
+def solution_01(path="input.data"):
+    return sum(read_data(path))
+
+
+def solution_02(path="input.data"):
+    unique_frequencies = set()
+    for frequency in accumulate(cycle(read_data(path)), initial=0):
+        if frequency in unique_frequencies:
+            return frequency
+        unique_frequencies.add(frequency)
+
+
+if __name__ == "__main__":
+    print("Solution 01:", solution_01())
+    print("Solution 02:", solution_02())
