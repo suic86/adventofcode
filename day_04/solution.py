81866336d26c39a8ee5f860a73b05a63616865a6
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-09 17:00:37 +0100

    Day 04 - Solutions - refactoring

--- day_04/solution.py
+++ day_04/solution.py
@@ -35,24 +35,26 @@ def value_in_range(value, lower, upper):
 
 
 def is_byr_valid(year):
-    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    """byr (Birth Year) - four digits; at least 1920 and at most 2002."""
     return value_in_range(year, 1920, 2002)
 
 
 def is_iyr_valid(year):
-    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    """iyr (Issue Year) - four digits; at least 2010 and at most 2020."""
     return value_in_range(year, 2010, 2020)
 
 
 def is_eyr_valid(year):
-    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    """eyr (Expiration Year) - four digits; at least 2020 and at most 2030."""
     return value_in_range(year, 2020, 2030)
 
 
 def is_hgt_valid(height):
-    # hgt (Height) - a number followed by either cm or in:
-    #     If cm, the number must be at least 150 and at most 193.
-    #     If in, the number must be at least 59 and at most 76.
+    """
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    """
     m = re.match(r"(\d+)(in|cm)", height)
     if m is None:
         return False
@@ -66,22 +68,22 @@ def is_hgt_valid(height):
 
 
 def is_hcl_valid(hair_color):
-    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    """hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f."""
     return bool(re.match("^#[0-9a-f]{6}$", hair_color))
 
 
 def is_ecl_valid(eye_color):
-    # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    """ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth."""
     return eye_color in "amb blu brn gry grn hzl oth".split()
 
 
 def is_pid_valid(passport_id):
-    # pid (Passport ID) - a nine-digit number, including leading zeroes.
+    """pid (Passport ID) - a nine-digit number, including leading zeroes."""
     return bool(re.match("^[0-9]{9}$", passport_id))
 
 
 def is_cid_valid(country_id):
-    # cid (Country ID) - ignored, missing or not.
+    """cid (Country ID) - ignored, missing or not."""
     return True
 
 
@@ -124,5 +126,5 @@ def solution_02(path="input.data"):
 
 
 if __name__ == "__main__":
-    print(solution_01())
-    print(solution_02())
+    print("Solution 01:", solution_01())
+    print("Solution 02:", solution_02())
---
+++ day_04/test_solution.py
@@ -0,0 +1,17 @@
+import pytest
+
+from solution import solution_01, solution_02
+
+
+@pytest.mark.parametrize(
+    "source,result", [("test_solution_01.data", 2), ("input.data", 242)]
+)
+def test_solution_01(source, result):
+    assert solution_01(source) == result
+
+
+@pytest.mark.parametrize(
+    "source,result", [("test_solution_02.data", 4), ("input.data", 186)]
+)
+def test_solution_02(source, result):
+    assert solution_02(source) == result
