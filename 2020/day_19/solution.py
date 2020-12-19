df4c9fecb069f49357a87754fea1f8ba1b040952
Gabriel Marko <marko.gabriel.cz@gmail.com>
2020-12-19 23:22:22 +0100

    Day 19 - Solutions

---
+++ 2020/day_19/solution.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import re
+
+from functools import lru_cache
+from itertools import product
+
+
+def read_data(path="input.data"):
+    rules = {}
+    data = []
+    with open(path) as fobj:
+        rule_section = True
+        for line in map(str.rstrip, fobj):
+            if not line:
+                rule_section = False
+                continue
+            if rule_section:
+                rule_number, rule = line.split(": ")
+                rules[int(rule_number)] = rule.strip('"')
+            else:
+                data.append(line)
+    return rules, data
+
+
+def convert_to_regex(rule, rules):
+    terminals = "ab"
+
+    rule_number = rule
+    rule = rules[rule]
+
+    if rule in terminals:
+        return rule
+
+    return "(?:%s)" % "|".join(
+        "".join(convert_to_regex(int(r), rules) for r in rl.split())
+        for rl in rule.split(" | ")
+    )
+
+
+def solution_02_check(line, rule42, rule31):
+    """
+    Rule 8:  42    | 42 8      # rule 42 must appear one or more times
+    Rule 11: 42 31 | 42 11 31  # rule 42 and rule 31 must appear n times where n > 0
+    Rule 0:  8 11              # rule 8 and rule 11 must appear exactly one time
+
+    Rule 0 means that
+
+    1) rule 42 must appear at least twice (once in 8 and once in 11).
+    2) rule 42 must appear more times than rule 31.
+    """
+    matches = re.fullmatch(f"({rule42}{{2,}})({rule31}+)", line)
+    if matches is None:
+        return False
+    s42, s31 = matches.groups()
+    l42 = len(re.findall(rule42, s42))
+    l31 = len(re.findall(rule31, s31))
+    return l42 > l31
+
+
+def solution_01(path="input.data"):
+    rules, data = read_data(path)
+    rule0 = convert_to_regex(0, rules)
+    rule0 = re.compile(rule0)
+    return sum(rule0.fullmatch(d) is not None for d in data)
+
+
+def solution_02(path="input.data"):
+    rules, data = read_data(path)
+    rule42 = convert_to_regex(42, rules)
+    rule31 = convert_to_regex(31, rules)
+    return sum(solution_02_check(line, rule42, rule31) for line in data)
+
+
+if __name__ == "__main__":
+    print("Solution 01:", solution_01())
+    print("Solution 02:", solution_02())
---
+++ 2020/day_19/test_solution.py
@@ -0,0 +1,90 @@
+import re
+import pytest
+from solution import (
+    read_data,
+    convert_to_regex,
+    solution_01,
+    solution_02,
+    solution_02_check,
+)
+
+
+def test_read_data():
+    rules, data = read_data("test_solution_01.data")
+    assert rules == {
+        0: "4 1 5",
+        1: "2 3 | 3 2",
+        2: "4 4 | 5 5",
+        3: "4 5 | 5 4",
+        4: "a",
+        5: "b",
+    }
+    assert data == [
+        "ababbb",
+        "bababa",
+        "abbbab",
+        "aaabbb",
+        "aaaabbb",
+    ]
+
+
+def test_convert_to_regex():
+    rules = {0: "1 2 1", 1: "2 | 3", 2: "a", 3: "b"}
+    assert convert_to_regex(0, rules) == "(?:(?:a|b)a(?:a|b))"
+    rules = {0: "1 2 1", 1: "2 3", 2: "a", 3: "b"}
+    assert convert_to_regex(0, rules) == "(?:(?:ab)a(?:ab))"
+
+
+@pytest.mark.parametrize(
+    "message",
+    [
+        "bbabbbbaabaabba",
+        "babbbbaabbbbbabbbbbbaabaaabaaa",
+        "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+        "bbbbbbbaaaabbbbaaabbabaaa",
+        "bbbababbbbaaaaaaaabbababaaababaabab",
+        "ababaaaaaabaaab",
+        "ababaaaaabbbaba",
+        "baabbaaaabbaaaababbaababb",
+        "abbbbabbbbaaaababbbbbbaaaababb",
+        "aaaaabbaabaaaaababaa",
+        "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+        "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+    ],
+)
+def test_solution_02_check_test_data_expect_true(message):
+    rules, _ = read_data("test_solution_02.data")
+    r_42 = convert_to_regex(42, rules)
+    r_31 = convert_to_regex(31, rules)
+    assert solution_02_check(message, r_42, r_31) is True
+
+
+@pytest.mark.parametrize(
+    "message",
+    [
+        "aaaabbaaaabbaaa",
+        "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+        "babaaabbbaaabaababbaabababaaab",
+    ],
+)
+def test_solution_02_check_test_data_expect_false(message):
+    rules, _ = read_data("test_solution_02.data")
+    r_42 = convert_to_regex(42, rules)
+    r_31 = convert_to_regex(31, rules)
+    assert solution_02_check(message, r_42, r_31) is False
+
+
+@pytest.mark.parametrize(
+    "source,result",
+    [("test_solution_01.data", 2), ("test_solution_02.data", 3), ("input.data", 144)],
+)
+def test_solution_01(source, result):
+    assert solution_01(source) == result
+
+
+@pytest.mark.parametrize(
+    "source,result",
+    [("test_solution_02.data", 12), ("input.data", 260)],
+)
+def test_solution_02(source, result):
+    assert solution_02(source) == result
