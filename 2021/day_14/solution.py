5fedf2d59175379cf052d357d97dcdb21c3f084e
Gabriel Marko <marko.gabriel.cz@gmail.com>
2021-12-14 13:04:04 +0100

    2021 - Day 14 - Solutions.

---
+++ 2021/day_14/solution.py
@@ -0,0 +1,78 @@
+from collections import Counter, defaultdict
+from itertools import islice
+
+
+def load_data(path="input.data"):
+    with open(path) as fobj:
+        fobj = map(str.strip, fobj)
+        polymer_template = next(fobj)
+        # skip empty line
+        next(fobj)
+        insertion_rules = {tuple(k): v for k, v in (row.split(" -> ") for row in fobj)}
+    return polymer_template, insertion_rules
+
+
+# naive_ functions were used to solve the first part
+
+
+def naive_step_gen(polymer_template, insertion_rules):
+    state = list(polymer_template)
+    while True:
+        new_state = []
+        for pair in zip(state, state[1:]):
+            if pair in insertion_rules:
+                new_state.extend([pair[0], insertion_rules[pair]])
+            else:
+                new_state.append(pair[0])
+        new_state.append(state[-1])
+        state = new_state
+        yield "".join(state)
+
+
+def naive_nth_step(n, polymer_template, insertion_rules):
+    return Counter(
+        next(islice(naive_step_gen(polymer_template, insertion_rules), n - 1, n))
+    )
+
+
+def naive_solution_01(path="input.data"):
+    element_counts = naive_nth_step(10, *load_data(path))
+    return max(element_counts.values()) - min(element_counts.values())
+
+
+def optimized_step_gen(polymer_template, insertion_rules):
+    elements = Counter(polymer_template)
+    state = Counter(zip(polymer_template, polymer_template[1:]))
+    while True:
+        new_state = defaultdict(int)
+        for pair, count in state.items():
+            if pair not in insertion_rules:
+                new_state[pair] = count
+                continue
+            left, right = pair
+            new_state[(left, insertion_rules[pair])] += count
+            new_state[(insertion_rules[pair], right)] += count
+            elements[insertion_rules[pair]] += count
+        state = new_state
+        yield elements
+
+
+def optimized_nth_step(n, polymer_template, insertion_rules):
+    if n == 0:
+        return Counter(polymer_template)
+    return next(islice(optimized_step_gen(polymer_template, insertion_rules), n - 1, n))
+
+
+def solution_01(path="input.data"):
+    element_counts = optimized_nth_step(10, *load_data(path))
+    return max(element_counts.values()) - min(element_counts.values())
+
+
+def solution_02(path="input.data"):
+    element_counts = optimized_nth_step(40, *load_data(path))
+    return max(element_counts.values()) - min(element_counts.values())
+
+
+if __name__ == "__main__":
+    print(f"Solution 01: {solution_01()}")
+    print(f"Solution 02: {solution_02()}")
---
+++ 2021/day_14/test_solution.py
@@ -0,0 +1,81 @@
+import pytest
+from collections import Counter
+from itertools import islice
+
+from solution import (
+    load_data,
+    naive_step_gen,
+    solution_01,
+    solution_02,
+    optimized_step_gen,
+)
+
+
+@pytest.fixture
+def testdata():
+    polymer_template = "NNCB"
+    insertion_rules = {
+        ("C", "H"): "B",
+        ("H", "H"): "N",
+        ("C", "B"): "H",
+        ("N", "H"): "C",
+        ("H", "B"): "C",
+        ("H", "C"): "B",
+        ("H", "N"): "C",
+        ("N", "N"): "C",
+        ("B", "H"): "H",
+        ("N", "C"): "B",
+        ("N", "B"): "B",
+        ("B", "N"): "B",
+        ("B", "B"): "N",
+        ("B", "C"): "B",
+        ("C", "C"): "N",
+        ("C", "N"): "C",
+    }
+    return polymer_template, insertion_rules
+
+
+def test_load_data(testdata):
+    assert load_data("test.data") == testdata
+
+
+@pytest.mark.parametrize(
+    "step,polymer",
+    [
+        (1, "NCNBCHB"),
+        (2, "NBCCNBBBCBHCB"),
+        (3, "NBBBCNCCNBBNBNBBCHBHHBCHB"),
+        (4, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"),
+    ],
+)
+def test_step_gen(step, polymer, testdata):
+    first_step = next(islice(naive_step_gen(*testdata), step - 1, step))
+    assert first_step == polymer
+
+
+@pytest.mark.parametrize(
+    "step,polymer",
+    [
+        (1, "NCNBCHB"),
+        (2, "NBCCNBBBCBHCB"),
+        (3, "NBBBCNCCNBBNBNBBCHBHHBCHB"),
+        (4, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"),
+    ],
+)
+def test_optimized_step_gen(step, polymer, testdata):
+    nth_step = next(islice(optimized_step_gen(*testdata), step - 1, step))
+    assert nth_step == Counter(polymer)
+
+
+@pytest.mark.parametrize(
+    "source_data,result", [("test.data", 1588), ("input.data", 2891)]
+)
+def test_solution_01(source_data, result):
+    assert solution_01(source_data) == result
+
+
+@pytest.mark.parametrize(
+    "source_data,result", [("test.data", 2188189693529), ("input.data", 4607749009683)]
+)
+def test_solution_02(source_data, result):
+    assert solution_02(source_data) == result
