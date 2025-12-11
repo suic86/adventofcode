b2f864eb2f29ab4d6efe583695e5fd88cced0030
suic86 <marko.gabriel.cz@gmail.com>
2025-12-11 20:47:55 +0100

    refactoring

--- 2025/day_11/solution.py
+++ 2025/day_11/solution.py
@@ -1,60 +1,11 @@
 from collections import defaultdict
 from graphlib import TopologicalSorter
-from typing import Callable, Mapping
+from typing import Mapping
 
 Graph = Mapping[str, set[str]]
 
 
-def parse_data(path: str = "input.data") -> Graph:
-    data = defaultdict(set)
-    with open(path) as fp:
-        for line in map(str.strip, fp):
-            n, a = line.split(": ")
-            data[n] = data[n].union(set(a.split()))
-    return data
-
-
-def dfs(
-    g: Graph,
-    s: str,
-    e: str,
-    visited: set[str],
-    current_path: list[str],
-    constraint: Callable[[list[str]], bool] | None = None,
-) -> int:
-    if s in visited:
-        return 0
-
-    visited.add(s)
-    current_path.append(s)
-
-    if s == e:
-        visited.remove(s)
-        current_path.pop()
-        print(current_path)
-        return constraint is None or constraint(current_path)
-
-    paths = sum(dfs(g, n, e, visited, current_path, constraint) for n in g[s])
-
-    current_path.pop()
-    visited.remove(s)
-    return paths
-
-
-def simple_paths(
-    graph: Graph,
-    start_node: str,
-    end_node: str,
-    constraint: Callable[[list[str]], bool] | None = None,
-) -> int:
-    current_path = []
-    visited = set()
-    return dfs(
-        graph, start_node, end_node, visited, current_path, constraint=constraint
-    )
-
-
-def invert_graph(graph: Graph):
+def invert_graph(graph: Graph) -> Graph:
     inverted = defaultdict(set)
     for k, v in graph.items():
         for e in v:
@@ -62,29 +13,39 @@ def invert_graph(graph: Graph):
     return inverted
 
 
-def count_path(graph: Graph, source, destination) -> int:
+def parse_data(path: str = "input.data") -> Graph:
+    data = defaultdict(set)
+    with open(path) as fp:
+        for line in map(str.strip, fp):
+            n, a = line.split(": ")
+            data[n] = data[n].union(a.split())
+    return data
 
-    top_order = tuple(TopologicalSorter(invert_graph(graph)).static_order())
 
+def topological_order(graph: Graph) -> tuple:
+    return tuple(TopologicalSorter(invert_graph(graph)).static_order())
+
+
+def count_path(graph: Graph, topo_order, source, destination) -> int:
     ways = defaultdict(int)
     ways[source] = 1
-    for node in top_order:
+    for node in topo_order:
         for ng in graph[node]:
             ways[ng] += ways[node]
-
     return ways[destination]
 
 
 def solution_01(path: str = "input.data") -> int:
     graph = parse_data(path)
-    return count_path(graph, "you", "out")
+    return count_path(graph, topological_order(graph), "you", "out")
 
 
 def solution_02(path: str = "input.data") -> int:
     graph = parse_data(path)
-    sf = count_path(graph, "svr", "fft")
-    fd = count_path(graph, "fft", "dac")
-    do = count_path(graph, "dac", "out")
+    topo_order = topological_order(graph)
+    sf = count_path(graph, topo_order, "svr", "fft")
+    fd = count_path(graph, topo_order, "fft", "dac")
+    do = count_path(graph, topo_order, "dac", "out")
 
     res = sf * fd * do
     return res
