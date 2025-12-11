7b09472780b3275577bdba96276b678335577786
suic86 <marko.gabriel.cz@gmail.com>
2025-12-11 21:46:55 +0100

    refactoring

--- 2025/day_11/solution.py
+++ 2025/day_11/solution.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 from graphlib import TopologicalSorter
+from math import prod
 from typing import Mapping
 
-Graph = Mapping[str, set[str]]
+Node = str
+Graph = Mapping[str, set[Node]]
 
 
 def invert_graph(graph: Graph) -> Graph:
@@ -22,37 +24,45 @@ def parse_data(path: str = "input.data") -> Graph:
     return data
 
 
-def topological_order(graph: Graph) -> tuple:
-    return tuple(TopologicalSorter(invert_graph(graph)).static_order())
+def topological_order(graph: Graph) -> list[str]:
+    return list(TopologicalSorter(invert_graph(graph)).static_order())
 
 
-def count_path(graph: Graph, topo_order, source, destination) -> int:
-    ways = defaultdict(int)
-    ways[source] = 1
-    for node in topo_order:
-        for ng in graph[node]:
-            ways[ng] += ways[node]
-    return ways[destination]
+def count_path(
+    graph: Graph,
+    source: Node,
+    destination: Node,
+    intermediate: list[Node] | None = None,
+) -> int:
+    if intermediate is None:
+        intermediate = []
+    topo_order = topological_order(graph)
+    order = sorted([source, destination] + intermediate, key=topo_order.index)
+
+    if order[0] != source or order[-1] != destination:
+        return 0
+
+    def ways(source, destination):
+        ways = defaultdict(int)
+        ways[source] = 1
+        for node in topo_order:
+            for ng in graph[node]:
+                ways[ng] += ways[node]
+        return ways[destination]
+
+    return prod(map(ways, order, order[1:]))
 
 
 def solution_01(path: str = "input.data") -> int:
     graph = parse_data(path)
-    return count_path(graph, topological_order(graph), "you", "out")
+    return count_path(graph, "you", "out")
 
 
 def solution_02(path: str = "input.data") -> int:
     graph = parse_data(path)
-    topo_order = topological_order(graph)
-    sf = count_path(graph, topo_order, "svr", "fft")
-    fd = count_path(graph, topo_order, "fft", "dac")
-    do = count_path(graph, topo_order, "dac", "out")
-
-    res = sf * fd * do
-    return res
+    return count_path(graph, "svr", "out", ["fft", "dac"])
 
 
 if __name__ == "__main__":
-    from pprint import pprint
-
-    # pprint(solution_02("test_02.data"))
-    pprint(solution_01("input.data"))
+    print(f"Solution 01: {solution_01()}")
+    print(f"Solution 02: {solution_02()}")
